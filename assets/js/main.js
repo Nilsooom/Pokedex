98231fb38f8a7b398d3ae2bd0a41c24b8913b617
Nilsooom <nilsooom.b@gmail.com>
2023-05-17 00:57:33 -0300

    Adicionado os tipos dinamicamente, com alteração de cor de acordo com o tipo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,26 @@
-function covertListtoTypes(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>` )
-}
 
 function pokemonsToHTML(pokemon) {
     return `
-    <li class="pokemon">
-                <span class="number">${pokemon.order}</span>
-                <span class="name">${pokemon.name}</span>
-
-                <div class="detail">
-                    <ol class="types">
-                        ${covertListtoTypes(pokemon.types.join(''))}
-                    </ol>
-                    <img src="${pokemon.stripes.other.dream_wrold.front_deafult}" alt="${pokemon.name}">
-                </div>
-            </li>
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+    
+    
+    <div class="detail">
+        <ol class="types">
+        ${pokemon.types.map((tipo) => `<li class="type ${tipo}">${tipo}</li>`).join('')}
+        </ol>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+    </div>
+    </li>
     `
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-const offset = 0;
-const limit = 11;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-pokeApi.getPokemon().then( (pokemonsLista = []) => {
 
+pokeApi.getPokemon(0, 27).then((pokemonsLista = []) => {
     pokemonList.innerHTML += pokemonsLista.map(pokemonsToHTML).join('')
-})  
-.catch( (error) => {console.error(error)})
+})
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,23 @@
 
-
 const pokeApi = {} // objeto vazio para armazenar as funções
+
+function covertPokeApiDetailtoPokemon(pokeDetail) {
+const pokemon = new Pokemon()
+pokemon.name = pokeDetail.name
+pokemon.number = pokeDetail.order
+pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+pokemon.type =  pokeDetail.types[0].type.name
+pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+return pokemon
+}
 
 pokeApi.getPokemonDetail = (pokemon) => {
 return  fetch(pokemon.url).then( (response) => response.json())
+.then(covertPokeApiDetailtoPokemon)
 }
 
-pokeApi.getPokemon = (offset = 1, limit = 11) => {
+pokeApi.getPokemon = (offset = 0, limit = 11) => {
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
 return fetch(url)
@@ -15,5 +26,4 @@ return fetch(url)
 .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
 .then((detailRequests) => Promise.all(detailRequests))
 .then((responsePokemons) => responsePokemons)
-.catch( (error) => {console.error(error)}) 
 }
